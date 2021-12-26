78c4d8be86a17b66f87afb36487338ea56a8884a
csp <yemuc@qq.com>
2021-12-26 14:34:10 +0800

    init

---
+++ Notes/src/test/java/com/csp/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csp.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
